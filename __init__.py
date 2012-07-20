46a24534d8f8bfb043794b760c450a19e71961fd
Dave Paola <dpaola2@gmail.com>
2012-07-19 18:47:08 -0700

    some renaming, added filename to command JSON

--- __init__.py
+++ __init__.py
@@ -1,17 +1,17 @@
 import sublime, sublime_plugin, socket, json
 from diff_match_patch import diff_match_patch as dmp
-from editor import InfinoteEditor
+from editor import SubliminalEditor
 
 class EasyPair(sublime_plugin.EventListener):
     def __init__(self):
         self.previous_text = ""
-        self.editor = InfinoteEditor()
+        self.editor = SubliminalEditor()
         self.dmp = dmp()
-
+        
     def on_modified(self, view):
         text = view.substr(view.visible_region())
         diff = self.dmp.diff_main(self.previous_text, text)
+        filename = view.file_name()
         self.dmp.diff_cleanupSemantic(diff)
-        self.editor.sync(diff)
+        self.editor.sync(filename, diff)
         self.previous_text = text
-        
--- editor.py
+++ editor.py
@@ -2,27 +2,24 @@ import os, sys, json
 
 WEBUI_ROOT = os.path.dirname(os.path.realpath(__file__))[:-5]
 sys.path.append(WEBUI_ROOT)
-from infinote import *
+from subliminal import *
 
-example_log = [ ['i', (0,' ', 0, 'foobar foobar bar foo')], ]
-
-class InfinoteEditor(object):
-    
+class SubliminalEditor(object):
     def __init__(self):
         self._state = State()
 
-    def _handleInsert(self, offset, text):
+    def _handleInsert(self, filename, offset, text):
         buffer = Buffer([Segment(0, text)])
         operation = Insert(offset, buffer)
         request = DoRequest(0, self._state.vector, operation)
-        self._postCommand("insert", [0, str(request.vector), offset, text])
+        self._postCommand("insert", [0, str(request.vector), offset, text, filename])
         executedRequest = self._state.execute(request)
   
-    def _handleDelete(self, offset, text):
+    def _handleDelete(self, filename, offset, text):
         buffer = self._state.buffer.slice(offset, offset + len(text))
         operation = Delete(offset, buffer)
         request = DoRequest(0, self._state.vector, operation)
-        self._postCommand("delete", [0, str(request.vector), offset, len(text)])
+        self._postCommand("delete", [0, str(request.vector), offset, len(text), filename])
         executedRequest = self._state.execute(request)
         
     def _handleUndo(self, diffText):
@@ -33,23 +30,18 @@ class InfinoteEditor(object):
     def _postCommand(self, command, args):
         print json.dumps([command, args])
             
-    def sync(self, diffs):
+    def sync(self, filename, diffs):
         """ Takes a diff array and applies it to the buffer."""
         offset = 0
         for diff in diffs:
             diffType = diff[0]
             diffText = diff[1]
             if diffType == 1:
-                self._handleInsert(offset, diffText)
+                self._handleInsert(filename, offset, diffText)
                 offset += len(diffText)
             elif diffType == -1:
-                self._handleDelete(offset, diffText)
+                self._handleDelete(filename, offset, diffText)
             elif diffType == -2: # this is never used
                 self._handleUndo(diffText) # deprecated
             else:
                 offset += len(diffText)
-
-def main():
-    editor = InfinoteEditor()
-    editor.sync(example_log)
-    print editor._state.buffer
